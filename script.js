488f0d7ebade58c29391be3c832ac6de68dba663
Svet_lana_17 <ocelot_17@mail.ru>
2023-05-08 22:02:09 +0600

    созданы переменные и алерт в script

---
+++ script.js
@@ -0,0 +1,8 @@
+const title = "Book title";
+let  screens ='url of screens';
+let screenPrice = 458;
+let rollback = 'git checkout id commit';
+const fullPrice = 500;
+let adaptive ='adaptive subdiv';
+alert('Проверьте правильность заполнения полей');
+console.log('Привет');
